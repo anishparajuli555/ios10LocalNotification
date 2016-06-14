ff3239ded97ece0c7128a3ff09ce6c60236fb744
Anish Parajuli <anish.parajuli555@gmail.com>
2016-06-14 22:21:28 +0545

    Sample Project for Local notification in iOS 10.

---
+++ LocalNotificationSample/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  LocalNotificationSample
+//
+//  Created by Anish on 6/14/16.
+//  Copyright © 2016 Anish. All rights reserved.
+//
+
+import UIKit
+import  UserNotifications
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction  func triggerNotification(){
+        
+        let content = UNMutableNotificationContent()
+        content.title = NSString.localizedUserNotificationString(forKey: "Hello!", arguments: nil)
+        content.body = NSString.localizedUserNotificationString(forKey: "We are live at WWDC", arguments: nil)
+        content.sound = UNNotificationSound.default()
+
+        // Deliver the notification in five seconds.
+        let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 1.0, repeats: false)
+        let request = UNNotificationRequest.init(identifier: "FiveSecond", content: content, trigger: trigger)
+        
+        // Schedule the notification.
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.add(request)
+    
+        
+    }
+    
+    
+    
+    @IBAction func stopNotification(_ sender: AnyObject) {
+        
+        let center = UNUserNotificationCenter.current()
+        center.removePendingNotificationRequests(withIdentifiers: ["FiveSecond"])
+        
+    }
+    
+
+
+}
+
+extension ViewController:UNUserNotificationCenterDelegate{
+
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: () -> Void) {
+        
+        print("Tapped in notification")
+        
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: (UNNotificationPresentationOptions) -> Void) {
+        
+        print("Notification being triggered")
+        //You can either present alert ,sound or badge while the app is in foreground too with ios 10
+        completionHandler( UNNotificationPresentationOptions.alert)
+        //completionHandler( UNNotificationPresentationOptions.alert)
+        //completionHandler( UNNotificationPresentationOptions.alert)
+        
+    }
+  
+}
