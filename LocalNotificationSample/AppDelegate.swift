98cbda6c17c00078006031c2c13706c203558a13
Anish Parajuli <anish.parajuli555@gmail.com>
2016-06-16 10:58:01 +0545

    FIX: Minor changes in code for swift 3.

--- LocalNotificationSample/AppDelegate.swift
+++ LocalNotificationSample/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //request authorization
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization([.alert, .sound]) { (granted, error) in
             // Enable or disable features based on authorization.
--- LocalNotificationSample/ViewController.swift
+++ LocalNotificationSample/ViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 import  UserNotifications
+import UserNotificationsUI //framework to customize the notification
 
 class ViewController: UIViewController {
-
+   
+    let requestIdentifier = "SampleRequest"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,28 +26,35 @@ class ViewController: UIViewController {
     @IBAction  func triggerNotification(){
         
         let content = UNMutableNotificationContent()
-        content.title = NSString.localizedUserNotificationString(forKey: "Hello!", arguments: nil)
-        content.body = NSString.localizedUserNotificationString(forKey: "We are live at WWDC", arguments: nil)
+        content.title = "Intro to Notifications"
+        content.subtitle = "Lets code,Talk is cheap"
+        content.body = "Sample code from WWDC"
         content.sound = UNNotificationSound.default()
 
         // Deliver the notification in five seconds.
+      
         let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 1.0, repeats: false)
-        let request = UNNotificationRequest.init(identifier: "FiveSecond", content: content, trigger: trigger)
+        let request = UNNotificationRequest(identifier:requestIdentifier, content: content, trigger: trigger)
         
-        // Schedule the notification.
-        let center = UNUserNotificationCenter.current()
-        center.delegate = self
-        center.add(request)
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().add(request){(error) in
+            
+            if (error != nil){
+            
+                //handle here
+            
+            }
+            
+        }
     
         
     }
-    
-    
-    
+
     @IBAction func stopNotification(_ sender: AnyObject) {
         
         let center = UNUserNotificationCenter.current()
-        center.removePendingNotificationRequests(withIdentifiers: ["FiveSecond"])
+        center.removePendingNotificationRequests(withIdentifiers: [requestIdentifier])
+
         
     }
     
@@ -66,9 +75,14 @@ extension ViewController:UNUserNotificationCenterDelegate{
         
         print("Notification being triggered")
         //You can either present alert ,sound or increase badge while the app is in foreground too with ios 10
-         completionHandler( UNNotificationPresentationOptions.alert)
-        // completionHandler( UNNotificationPresentationOptions.sound)
-        // completionHandler( UNNotificationPresentationOptions.badge)
+        //to distinguish between notifications
+        if notification.request.identifier == requestIdentifier{
+        
+         completionHandler( [.alert,.sound])
+        
+       
+        
+        }
         
     }
   
