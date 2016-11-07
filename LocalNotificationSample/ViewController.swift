50c88d71d6fdf8e7ace7011121ab4d9303930609
Anish Parajuli <anish.smartmobe@gmail.com>
2016-11-07 12:58:31 +0545

    Fix: Updated example for Xcode 8

--- LocalNotificationSample/ViewController.swift
+++ LocalNotificationSample/ViewController.swift
@@ -11,79 +11,81 @@ import  UserNotifications
 import UserNotificationsUI //framework to customize the notification
 
 class ViewController: UIViewController {
-   
-    let requestIdentifier = "SampleRequest"
+    
+    let requestIdentifier = "SampleRequest" //identifier is to cancel the notification request
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
     
     @IBAction  func triggerNotification(){
         
+        print("notification will be triggered in five seconds..Hold on tight")
         let content = UNMutableNotificationContent()
         content.title = "Intro to Notifications"
         content.subtitle = "Lets code,Talk is cheap"
         content.body = "Sample code from WWDC"
         content.sound = UNNotificationSound.default()
-
+        
+        //To Present image in notification
+        if let path = Bundle.main.path(forResource: "monkey", ofType: "png") {
+            let url = URL(fileURLWithPath: path)
+            
+            do {
+                let attachment = try UNNotificationAttachment(identifier: "sampleImage", url: url, options: nil)
+                content.attachments = [attachment]
+            } catch {
+                print("attachment not found.")
+            }
+        }
+        
         // Deliver the notification in five seconds.
-      
-        let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 1.0, repeats: false)
+        let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 5.0, repeats: false)
         let request = UNNotificationRequest(identifier:requestIdentifier, content: content, trigger: trigger)
         
         UNUserNotificationCenter.current().delegate = self
         UNUserNotificationCenter.current().add(request){(error) in
             
             if (error != nil){
-            
-                //handle here
-            
+                
+                print(error?.localizedDescription)
             }
-            
         }
-    
-        
     }
-
+    
     @IBAction func stopNotification(_ sender: AnyObject) {
         
+        print("Removed all pending notifications")
         let center = UNUserNotificationCenter.current()
         center.removePendingNotificationRequests(withIdentifiers: [requestIdentifier])
-
         
     }
-    
-
-
 }
 
 extension ViewController:UNUserNotificationCenterDelegate{
-
-
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: () -> Void) {
+    
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         
         print("Tapped in notification")
-        
     }
     
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: (UNNotificationPresentationOptions) -> Void) {
+    //This is key callback to present notification while the app is in foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         
         print("Notification being triggered")
         //You can either present alert ,sound or increase badge while the app is in foreground too with ios 10
         //to distinguish between notifications
         if notification.request.identifier == requestIdentifier{
-        
-         completionHandler( [.alert,.sound])
-        
-       
-        
+            
+            completionHandler( [.alert,.sound,.badge])
+            
         }
-        
     }
-  
 }
